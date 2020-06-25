d76fc1fbaf02d4727db3c9c1a513e8f54c800741
delaissement <19biy93@gmail.com>
2020-06-25 15:52:07 +0900

    revised error causing debugging codes

--- src/heap.c
+++ src/heap.c
@@ -255,14 +255,7 @@ dl_force_inline void dl_free_impl(struct malloc_state *state, struct malloc_chun
                         set_chunk_tag(next_chunk(p), new_tag);
                     }
                     /* tmte edit ends */
-                    if(((next_chunk(next)->head & PREV_INUSE_BIT) == PREV_INUSE_BIT)){
-                        //
-                        //
-                        //
-                        int i=0;
-                        i++;
-                    }
-                    size_t nnhead1 = next_chunk(next)->head;
+
 
                     if (next == state->top) {
                         size_t tsize = state->top_size += psize;
@@ -289,7 +282,6 @@ dl_force_inline void dl_free_impl(struct malloc_state *state, struct malloc_chun
                     }
                     else {
                         size_t nsize = chunk_size(next);
-                        size_t nnhead = next_chunk(next)->head; //debugging
                         psize += nsize;
                         unlink_chunk(state, next, nsize);
                         p->prev_foot |= (next->prev_foot & NEXT_EXH_BIT);
@@ -560,14 +552,6 @@ void dl_free_impl2(struct malloc_state* state, struct malloc_chunk* p){
                         set_chunk_tag(next_chunk(p), new_tag);
                     }
                     /* tmte edit ends */
-                    if(((next_chunk(next)->head & PREV_INUSE_BIT) == PREV_INUSE_BIT)){
-                        //
-                        //
-                        //
-                        int i=0;
-                        i++;
-                    }
-                    size_t nnhead1 = next_chunk(next)->head;
 
                     if (next == state->top) {
                         size_t tsize = state->top_size += psize;
@@ -594,7 +578,6 @@ void dl_free_impl2(struct malloc_state* state, struct malloc_chunk* p){
                     }
                     else {
                         size_t nsize = chunk_size(next);
-                        size_t nnhead = next_chunk(next)->head; //debugging
                         psize += nsize;
                         unlink_chunk(state, next, nsize);
                         p->prev_foot |= (next->prev_foot & NEXT_EXH_BIT);
